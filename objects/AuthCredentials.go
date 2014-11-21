f2e89aa7f5464b65518f68e458f12dd7821fdee0
SkaveRat <skaverat@funlynx.de>
2014-11-21 20:15:24 +0100

    refactoring

--- objects/AuthCredentials.go
+++ objects/AuthCredentials.go
@@ -14,9 +14,7 @@ func (a *AuthCredentials) Username() []byte {
 }
 
 func (a *AuthCredentials) PasswordString() string {
-	foo,_ := base64.StdEncoding.DecodeString(string(a.RawInput))
-	value := bytes.Split(foo, []byte("\x00"))
-	return string(value[2])
+	return string(a.Password())
 }
 
 func (a *AuthCredentials) Password() []byte {
